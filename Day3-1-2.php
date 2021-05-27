4fad6bcdda1f8f013c393da0370fe4bf0a967164
Dhumil Vora <67314868+dhumilvora1106@users.noreply.github.com>
2021-05-27 15:09:32 +0530

    Create Day3-1-2.php

---
+++ Day3-1-2.php
@@ -0,0 +1,13 @@
+<html>  
+   <body>  
+     Your Entered Details are...<br/>
+     <table>
+      <tr><td>Your Name is </td><td><?php echo $_POST["data1"]; ?> <br/></td></tr>
+      <tr><td>Your age </td><td><?php echo $_POST["data2"]; ?> <br/></td></tr>
+      <tr><td>Your Gender is </td><td><?php echo $_POST["data3"]; ?> <br/></td></tr>
+      <tr><td>Your City </td><td><?php echo $_POST["data4"]; ?><br/></td></tr>
+      <tr><td>Your Phone Number is </td><td><?php echo $_POST["data5"]; ?><br/></td></tr>
+      <tr><td>Your Email Id is </td><td><?php echo $_POST["data6"]; ?></td></tr>
+  </table>
+   </body>  
+</html> 
