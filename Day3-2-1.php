1ea4e3bba47e2210da5d9829902d5029c52eec6a
Dhumil Vora <67314868+dhumilvora1106@users.noreply.github.com>
2021-05-27 16:19:15 +0530

    Create Day3-2-1.php

---
+++ Day3-2-1.php
@@ -0,0 +1,32 @@
+<html>
+<body>
+    <form action = "Day-3-2-2.php" method = "post">  
+<table border="0">
+<tr>
+<td> <input type="text" name="num1" value="" placeholder="Enter English marks"/>
+</td>
+</tr>
+<tr>
+<td> <input type="text" name="num2" value="" placeholder="Enter Maths marks"/>
+</td>
+</tr>
+<tr>
+<td> <input type="text" name="num3" value="" placeholder="Enter Science marks"/>
+</td>
+</tr>
+<tr>
+<td> <input type="text" name="num4" value="" placeholder="Enter Gujarati marks"/>
+</td>
+</tr>
+<tr>
+<td> <input type="text" name="num5" value="" placeholder="Enter Physics marks"/>
+</td>
+</tr>
+<tr>
+<td> <input type="submit" name="submit" value="Submit"/>
+</td>
+</tr>
+</table>
+</form>
+</body>
+</html>
