1956532dfcaebf15c7ad2d7bb84900c595a9c3fa
Dhumil Vora <67314868+dhumilvora1106@users.noreply.github.com>
2021-05-27 15:08:17 +0530

    Create Day3-1-1.php

---
+++ Day3-1-1.php
@@ -0,0 +1,17 @@
+<html>  
+       <body>  
+         
+          <form action = "Day3-1-2.php" method = "post">  
+              <table border="1">
+                  <tr><td>Name </td><td><input type = "text" name = "data1" /> <br></td></tr>  
+                  <tr><td>Age </td><td><input type = "number" name = "data2" /> <br></td></tr>
+                  <tr><td>Gender </td><td><input type = "text" name = "data3" /> <br></td></tr>  
+                  <tr><td>City </td><td><input type = "text" name = "data4" /> <br></td></tr>
+                  <tr><td>Phone No.</td><td><input type = "number" name = "data5" /> <br></td></tr>  
+                  <tr><td>Email</td><td><input type = "email" name = "data6" /> <br></td></tr>
+             <tr><td><input type = "submit"/><td><input type = "reset" /></td></tr> 
+             </table>
+          </form>  
+            
+       </body>  
+    </html>  
